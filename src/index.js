64b8367d17a2585d46f3a48213b09bba945e76e3
Drekavac <diesel39931@gmail.com>
2019-04-13 20:41:24 +0600

    added swapiservice

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
-console.log('Hello World!!!');
+class SwapiService {
+
+    _apiBase = 'https://swapi.co/api';
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if(!res.ok) {
+            throw new Error(`Could not fetch ${url}, received ${res.status}`)
+        }
+        return await res.json();
+    }
+
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`);
+        return res.results
+    }
+
+    getPerson(id) {
+        return this.getResource(`/people/${id}`)
+    }
+
+    async getAllPlanet() {
+        const res = await this.getResource(`/planets/`);
+        return res.results
+    }
+
+    getPlanet(id) {
+        return this.getResource(`/planets/${id}`)
+    }
+
+    async getAllStarships() {
+        const res = await this.getResource(`/starships/`);
+        return res.results
+    }
+
+    getStarships(id) {
+        return this.getResource(`/starships/${id}`)
+    }
+
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((people) => {
+    people.forEach((p) => {
+        console.log(p.name)
+    })
+});
+
+swapi.getPerson(3).then((p) => {
+    console.log(p.name)
+});
+
+swapi.getAllPlanet()
+    .then((planets) => planets.results)
+    .then((results) => console.log(results));
